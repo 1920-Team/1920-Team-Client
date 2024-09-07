4ff1ab897c153dcaf12713a0a96f69f81c124c4d
niche0905 <yudle537@gmail.com>
2024-09-07 18:31:44 +0900

    세션 이름 나타내기
    leave 안되서 수정
    하지만 Join이 안됨

--- Source/Proto1920/GameInfoInstance.cpp
+++ Source/Proto1920/GameInfoInstance.cpp
@@ -3,7 +3,13 @@
 
 #include "GameInfoInstance.h"
 
+#include "Engine/Engine.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Blueprint/UserWidget.h"
+#include "OnlineSubsystem.h"
 #include "OnlineSessionSettings.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Online/OnlineSessionNames.h"
 
 
 const static FName SESSION_NAME = TEXT("My Session Game");
@@ -16,13 +22,39 @@ void UGameInfoInstance::Init()
 	if (Subsystem != nullptr) {
 		UE_LOG(LogTemp, Warning, TEXT("Found subsystem %s"), *Subsystem->GetSubsystemName().ToString());
 		SessionInterface = Subsystem->GetSessionInterface();
+		if (SessionInterface.IsValid()) {
+			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UGameInfoInstance::OnCreateSessionComplete);
+			SessionInterface->OnDestroySessionCompleteDelegates.AddUObject(this, &UGameInfoInstance::OnDestroySessionComplete);
+			SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this, &UGameInfoInstance::OnFindSessionsComplete);
+			SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this, &UGameInfoInstance::OnJoinSessionComplete);
+		}
 	}
 	else {
 		UE_LOG(LogTemp, Warning, TEXT("Found no subsystem"));
 	}
 }
 
-void UGameInfoInstance::CreateSessionByName(class APlayerController* PlayerController, int32 PublicConnections, bool UseLAN, bool UseLobbiesifAvailable, FString ServerName)
+void UGameInfoInstance::HostSession(int32 inPublicConnections, bool inUseLAN, bool inUseLobbiesifAvailable, FString inServerName)
+{
+	PublicConnections = inPublicConnections;
+	UseLAN = inUseLAN;
+	UseLobbiesifAvailable = inUseLobbiesifAvailable;
+	ServerName = inServerName;
+
+	if (SessionInterface.IsValid()) {
+		auto ExistingSession = SessionInterface->GetNamedSession(SESSION_NAME);
+		if (ExistingSession != nullptr)
+		{
+			SessionInterface->DestroySession(SESSION_NAME);
+		}
+		else
+		{
+			CreateSession();
+		}
+	}
+}
+
+void UGameInfoInstance::CreateSession()
 {
 	FOnlineSessionSettings SessionSettings;
 
@@ -41,4 +73,106 @@ void UGameInfoInstance::CreateSessionByName(class APlayerController* PlayerContr
 	SessionSettings.Set(SERVER_NAME_SETTINGS_KEY, ServerName, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
 
 	SessionInterface->CreateSession(0, SESSION_NAME, SessionSettings);
+}
+
+void UGameInfoInstance::OnCreateSessionComplete(FName SessionName, bool Success)
+{
+	if (!Success)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Could not create session"));
+		return;
+	}
+
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	World->ServerTravel("/Game/Maps/Lobby?listen");
+}
+
+void UGameInfoInstance::OnDestroySessionComplete(FName SessionName, bool Success)
+{
+	if (Success) {
+		CreateSession();
+	}
+}
+
+void UGameInfoInstance::FindSession(bool inUseLAN)
+{
+	SessionSearch = MakeShareable(new FOnlineSessionSearch());
+	if (SessionSearch.IsValid())
+	{
+		SessionSearch->bIsLanQuery = inUseLAN;
+		SessionSearch->MaxSearchResults = 100;
+		SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
+		UE_LOG(LogTemp, Warning, TEXT("Starting Find Session"));
+		SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
+	}
+}
+
+void UGameInfoInstance::OnFindSessionsComplete(bool Success)
+{
+	if (Success and SessionSearch.IsValid())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Finished Find Session"));
+
+		ServerDatas.Empty();
+
+		for (const FOnlineSessionSearchResult& SearchResult : SessionSearch->SearchResults)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Found session name: %s"), *SearchResult.GetSessionIdStr());
+			FServerData Data;
+			Data.MaxPlayers = SearchResult.Session.SessionSettings.NumPublicConnections;
+			Data.CurrentPlayers = Data.MaxPlayers - SearchResult.Session.NumOpenPublicConnections;
+			Data.HostUserName = SearchResult.Session.OwningUserName;
+			FString ViewServerName;
+			if (SearchResult.Session.SessionSettings.Get(SERVER_NAME_SETTINGS_KEY, ViewServerName))
+			{
+				Data.Name = ViewServerName;
+			}
+			else
+			{
+				Data.Name = "Could not find name.";
+			}
+
+			ServerDatas.Add(Data);
+		}
+
+		// TODO Call BP Setup (ServerRow)
+		OnSessionsFound(ServerDatas);
+	}
+}
+
+void UGameInfoInstance::JoinSession(int32 inIndex)
+{
+	if (!SessionInterface.IsValid()) return;
+	if (!SessionSearch.IsValid()) return;
+
+	SessionInterface->JoinSession(0, SESSION_NAME, SessionSearch->SearchResults[inIndex]);
+}
+
+void UGameInfoInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	if (!SessionInterface.IsValid()) return;
+
+	FString Address;
+	if (!SessionInterface->GetResolvedConnectString(SessionName, Address))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Could not get connect string."));
+		return;
+	}
+
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
 }
--- Source/Proto1920/GameInfoInstance.h
+++ Source/Proto1920/GameInfoInstance.h
@@ -9,6 +9,22 @@
 
 #include "GameInfoInstance.generated.h"
 
+
+USTRUCT(BlueprintType)
+struct FServerData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite)
+	FString Name;
+	UPROPERTY(BlueprintReadWrite)
+	int32 CurrentPlayers;
+	UPROPERTY(BlueprintReadWrite)
+	int32 MaxPlayers;
+	UPROPERTY(BlueprintReadWrite)
+	FString HostUserName;
+};
+
 /**
  * 
  */
@@ -21,9 +37,32 @@ public:
 	virtual void Init();
 
 	UFUNCTION(BlueprintCallable, Category = "ServerSettings")
-	void CreateSessionByName(class APlayerController* PlayerController, int32 PublicConnections, bool UseLAN, bool UseLobbiesifAvailable, FString ServerName);
+	void HostSession(int32 PublicConnections, bool UseLAN, bool UseLobbiesifAvailable, FString ServerName);
+
+	UFUNCTION(BlueprintCallable, Category = "SessionInteraciton")
+	void FindSession(bool UseLAN);
+
+	UFUNCTION(BlueprintCallable, Category = "SessionInteraciton")
+	void JoinSession(int32 Index);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "SessionInteraction")
+	void OnSessionsFound(const TArray<FServerData>& SessionInfos);
 
 private:
 	IOnlineSessionPtr SessionInterface;
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
+	TArray<FServerData> ServerDatas;
+
+	void OnCreateSessionComplete(FName SessionName, bool Success);
+	void OnDestroySessionComplete(FName SessionName, bool Success);
+	void OnFindSessionsComplete(bool Success);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+	void CreateSession();
+
+	int32 PublicConnections;
+	bool UseLAN;
+	bool UseLobbiesifAvailable;
+	FString ServerName;
 	
 };
